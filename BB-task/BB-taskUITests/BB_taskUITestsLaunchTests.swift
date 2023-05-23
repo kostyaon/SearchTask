b252f8fb79bf42677e587de1a070a4849797dde5
Konstantin Petrikevich <konstantin.petrikevich@gmail.com>
2023-05-23 16:22:00 +0200

    Initialize project

---
+++ BB-task/BB-taskUITests/BB_taskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BB_taskUITestsLaunchTests.swift
+//  BB-taskUITests
+//
+//  Created by KonstanTanos on 23/05/2023.
+//
+
+import XCTest
+
+final class BB_taskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
