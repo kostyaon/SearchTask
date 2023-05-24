6d523c5fe97f044184239a7f82a55a5ab23e9214
Konstantin Petrikevich <konstantin.petrikevich@gmail.com>
2023-05-24 09:27:28 +0200

    Add CityResponse model

---
+++ BB-task/BB-task/Models/CityResponse.swift
@@ -0,0 +1,38 @@
+//
+//  CityResponse.swift
+//  BB-task
+//
+//  Created by KonstanTanos on 23/05/2023.
+//
+
+import Foundation
+
+struct Coordinate: Decodable {
+    
+    let longitude: Double
+    let latitude: Double
+    
+    private
+    enum CodingKeys: String, CodingKey {
+        
+        case longitude = "long"
+        case latitude = "lat"
+    }
+}
+
+struct CityResponse: Decodable {
+    
+    let country: String
+    let name: String
+    let id: Int
+    let coordinates: Coordinate
+    
+    private
+    enum CodingKeys: String, CodingKey {
+        
+        case country
+        case name
+        case id = "_id"
+        case coordinates = "coord"
+    }
+}
