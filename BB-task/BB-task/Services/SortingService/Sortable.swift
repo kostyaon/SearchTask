ae6093ff7e2f10295969dc5ce5aff212faa3c0e1
Konstantin Petrikevich <konstantin.petrikevich@gmail.com>
2023-05-27 15:45:35 +0200

    Implement TypeErased AnySortable

--- BB-task/BB-task/Services/SortingService/Sortable.swift
+++ BB-task/BB-task/Services/SortingService/Sortable.swift
@@ -13,3 +13,16 @@ protocol Sortable {
     
     func sortAscending(_ array: inout [T])
 }
+
+struct AnySortable<T: Comparable>: Sortable {
+    
+    private let _sortAscending: (_ array: inout [T]) -> ()
+    
+    init<S: Sortable>(_ sortable: S) where S.T == T {
+        self._sortAscending = sortable.sortAscending
+    }
+    
+    func sortAscending(_ array: inout [T]) {
+        _sortAscending(&array)
+    }
+}
